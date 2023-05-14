beba2367269cfd836a020d7540957c6309f199b3
Zubenzitkar <zubenzitkar@gmail.com>
2023-05-14 17:51:48 +0300

    0.00.1 1) Development started

---
+++ app/src/main/java/com/example/sportsstatisticsapp/presentation/MainNavHost.kt
@@ -0,0 +1,11 @@
+package com.example.sportsstatisticsapp.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MainNavHost(){
+
+    val navController = rememberNavController()
+
+}
