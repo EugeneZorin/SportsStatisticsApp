448db6e752aaf7aa796fd14633b42ecb45ea3907
Zubenzitkar <zubenzitkar@gmail.com>
2023-06-04 13:06:02 +0300

    0.00.17

--- app/src/main/java/com/example/sportsstatisticsapp/presentation/screen/addworkout/AddNewWorkout.kt
+++ app/src/main/java/com/example/sportsstatisticsapp/presentation/screen/addworkout/AddNewWorkout.kt
@@ -5,14 +5,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
@@ -23,7 +22,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -100,7 +98,6 @@ fun NewWorkoutInputFields(
     modifier: Modifier = Modifier,
 ) {
 
-
     var nameWorkout by rememberSaveable { mutableStateOf("") }
     val shapeParameter = parameterResource(15, 15, 15, 15)
 
@@ -114,16 +111,26 @@ fun NewWorkoutInputFields(
     Box(
         modifier = modifier.shadow(31.dp),
     ) {
-        OutlinedTextField(
+        TextField(
             shape = shapeParameter,
             singleLine = true,
             value = nameWorkout,
             onValueChange = { nameWorkout = it },
+            textStyle = TextStyle(
+                textAlign = TextAlign.Center,
+                fontSize = 15.sp,
+                color = colorScheme.onSecondary
+            ),
+
             label = {
                 Text(
                     textFieldLabel,
-                    style = TextStyle(color = colorScheme.secondary),
-                    fontSize = 17.sp,
+                    modifier = Modifier.fillMaxWidth(1f),
+                    style = TextStyle(
+                        textAlign = TextAlign.Center,
+                        fontSize = 15.sp,
+                        color = colorScheme.secondary
+                    )
                 )
             },
             colors = customTextFieldColors,
