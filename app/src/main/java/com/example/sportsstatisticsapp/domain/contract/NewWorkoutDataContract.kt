8618a2344f288ca70e4f86f914c93e3236ed448d
Zubenzitkar <zubenzitkar@gmail.com>
2023-06-14 14:47:09 +0300

    0.00.25-a

--- app/src/main/java/com/example/sportsstatisticsapp/domain/contract/NewWorkoutDataContract.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sportsstatisticsapp.domain.contract
-
-interface NewWorkoutDataContract {
-    suspend fun addNewWorkoutTransferData(inputFieldMap: Map<String, String>)
-}
--- app/src/main/java/com/example/sportsstatisticsapp/presentation/viewmodel/addviewmodel/AddNewWorkoutViewModel.kt
+++ app/src/main/java/com/example/sportsstatisticsapp/presentation/viewmodel/addviewmodel/AddNewWorkoutViewModel.kt
@@ -2,8 +2,6 @@ package com.example.sportsstatisticsapp.presentation.viewmodel.addviewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.sportsstatisticsapp.domain.contract.NewWorkoutDataContract
-import com.example.sportsstatisticsapp.domain.usecase.NewWorkoutDataImpl
 import com.example.sportsstatisticsapp.presentation.navigation.bottomnavigation.buttoncontract.ButtonSaveContract
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -11,9 +9,10 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AddNewWorkoutViewModel @Inject constructor(
-    private val newWorkoutDataContract: NewWorkoutDataContract
+
 ) : ViewModel(), ButtonSaveContract {
-    constructor() : this(NewWorkoutDataImpl())
+
+
 
     private val inputFieldMap = mutableMapOf<String, String>()
     fun addNewWorkout(rowID: String, nameWorkout: String) {
